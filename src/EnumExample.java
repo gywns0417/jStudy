1f6fb781f5f254aed9803a34bcd6dff011e9ac6e
gywns0417 <gywns0417@gmail.com>
2023-04-24 19:53:01 +0900

    Enum 타입 예제, 테스트

---
+++ src/EnumExample.java
@@ -0,0 +1,9 @@
+public enum EnumExample {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
---
+++ src/EnumTest.java
@@ -0,0 +1,8 @@
+public class EnumTest {
+    public static void main(String[] args){
+        EnumExample today = EnumExample.SUNDAY;
+        EnumExample yesterday = EnumExample.SATURDAY;
+
+        if(today != yesterday) System.out.printf("어제는 %s, 오늘은 %s 입니다.", yesterday, today);
+    }
+}

f257a6d8d18200e3337eded005caebe5dfacd995
gywns0417 <gywns0417@gmail.com>
2023-04-27 16:25:14 +0900

    생성자, this 예제

---
+++ src/ConstructorExam.java
@@ -0,0 +1,15 @@
+public class ConstructorExam {
+    String nation = "대한민국";
+    String name;
+    String ssn;
+
+    public static void main(String[] args){
+
+    }
+
+    public ConstructorExam(String name, String ssn){
+        this.name = name;
+        this.ssn = ssn;
+
+    }
+}

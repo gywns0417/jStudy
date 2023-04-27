c0a0702687d4d2a3f506827cdcaa2c3690e085fe
gywns0417 <gywns0417@gmail.com>
2023-04-27 16:29:03 +0900

    생성자, this 예제 2

--- src/ConstructorExam.java
+++ src/ConstructorExam.java
@@ -4,12 +4,19 @@ public class ConstructorExam {
     String ssn;
 
     public static void main(String[] args){
+        ConstructorExam k1 = new ConstructorExam("킹효준", "000000-1234567");
+        ConstructorExam k2 = new ConstructorExam("킹투", "000001-1231411");
+
 
     }
 
     public ConstructorExam(String name, String ssn){
         this.name = name;
         this.ssn = ssn;
+        System.out.println(name);
+        System.out.println(ssn);
 
     }
+
+
 }

1aa140659b71c23cca49d196f7449fcac288f2a6
gywns0417 <gywns0417@gmail.com>
2023-05-09 17:10:15 +0900

    클래스 접근 제한 예제

---
+++ src/AccessModifier/AnotherPackage/C.java
@@ -0,0 +1,11 @@
+package AccessModifier.AnotherPackage;
+
+
+import AccessModifier.*;
+
+public class C {
+
+    A a; // import 후 public이라 접근 가능
+
+    // B b;    // import 했지만 public이 아니라 접근 불가
+}
---
+++ src/AccessModifier/B.java
@@ -0,0 +1,6 @@
+package AccessModifier;
+
+class B {
+    
+    A a;
+}

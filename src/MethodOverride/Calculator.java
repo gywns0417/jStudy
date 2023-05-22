87f120ca1d6fa714824d8289cb5a5bdc76d52577
gywns0417 <gywns0417@gmail.com>
2023-05-22 20:40:34 +0900

    메소드 오버라이딩 예제

---
+++ src/MethodOverride/Calculator.java
@@ -0,0 +1,9 @@
+package MethodOverride;
+
+public class Calculator {
+
+    public double areaCircle(double r){
+        System.out.println("Calculator 객체의 areaCircle() 실행");
+        return 3.14159 * r * r;
+    }
+}

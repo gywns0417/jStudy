4d8074e5ab9dd887c42aaf1050858e30cb009745
gywns0417 <gywns0417@gmail.com>
2023-05-23 19:45:37 +0900

    다형성 예제

---
+++ src/Polymorphism/CarExample.java
@@ -0,0 +1,18 @@
+package Polymorphism;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+
+        myCar.tire = new Tire();
+        myCar.run();
+
+        //HankookTire 객체 장착
+        myCar.tire = new HankookTire();
+        myCar.run();
+
+        //KumhoTire 객체 장착
+        myCar.tire = new KumhoTire();
+        myCar.run();
+    }
+}

3becbe9d1c1909c233166575ddcd43fcbfc47ff7
gywns0417 <gywns0417@gmail.com>
2023-06-09 20:26:16 +0900

    상속 확인문제 7,8번

---
+++ src/Questions/Inhertance/Q7/ChildExample.java
@@ -0,0 +1,7 @@
+package Questions.Inhertance.Q7;
+
+public class ChildExample {
+    public static void main(String[] args) {
+        Child child = new Child();
+    }
+}
---
+++ src/Questions/Inhertance/Q8/Tire.java
@@ -0,0 +1,7 @@
+package Questions.Inhertance.Q8;
+
+public class Tire {
+    public void run(){
+        System.out.println("일반 타이어가 굴러갑니다.");
+    }
+}

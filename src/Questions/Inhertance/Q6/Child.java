2ef65320cb7e9808ec8a2575e96091518b7bdb65
gywns0417 <gywns0417@gmail.com>
2023-06-09 20:00:27 +0900

    상속 확인문제 6번

---
+++ src/Questions/Inhertance/Q6/Child.java
@@ -0,0 +1,10 @@
+package Questions.Inhertance.Q6;
+
+public class Child extends Parent{
+    public int studentNo;
+
+    public Child(String name, int studentNo){
+        super(name);
+        this.studentNo = studentNo;
+    }
+}
---
+++ src/Questions/Inhertance/Q6/Parent.java
@@ -0,0 +1,9 @@
+package Questions.Inhertance.Q6;
+
+public class Parent {
+    public String name;
+
+    public Parent(String name) {
+        this.name = name;
+    }
+}

9735eaed47478fca27a8a10ef56db9546e8fd4a0
gywns0417 <gywns0417@gmail.com>
2023-06-12 15:59:39 +0900

    인터페이스 추상 메소드 예제

--- src/Interface/exam2/RemoteControl.java
+++ src/Interface/exam2/RemoteControl.java
@@ -3,4 +3,8 @@ package Interface.exam2;
 public interface RemoteControl {
     int MAX_VOLUME = 10;
     int MIN_VOLUME = 0;
+
+    void turnOn();
+    void turnOff();
+    void setVolume(int volume);
 }
--- src/Interface/exam2/RemoteControlExample.java
+++ src/Interface/exam2/RemoteControlExample.java
@@ -4,5 +4,17 @@ public class RemoteControlExample {
     public static void main(String[] args) {
         System.out.println("리모콘 최대 볼륨: " + RemoteControl.MAX_VOLUME);
         System.out.println("리모콘 최소 볼륨: " + RemoteControl.MIN_VOLUME);
+
+        RemoteControl rc;
+
+        rc = new Television();
+        rc.turnOn();
+        rc.setVolume(5);
+        rc.turnOff();
+
+        rc = new Audio();
+        rc.turnOn();
+        rc.setVolume(5);
+        rc.turnOff();
     }
 }

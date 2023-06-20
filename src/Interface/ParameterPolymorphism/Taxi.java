609a61aafa729e784325fb997266f69d5edcb6a1
gywns0417 <gywns0417@gmail.com>
2023-06-20 15:28:48 +0900

    인터페이스 매개변수 다형성 예제

---
+++ src/Interface/ParameterPolymorphism/Taxi.java
@@ -0,0 +1,10 @@
+package Interface.ParameterPolymorphism;
+
+public class Taxi implements Vehicle {
+
+    @Override
+    public void run() {
+        System.out.println("택시가 달립니다.");
+    }
+    
+}
---
+++ src/Interface/ParameterPolymorphism/Vehicle.java
@@ -0,0 +1,6 @@
+package Interface.ParameterPolymorphism;
+
+public interface Vehicle {
+    //추상 메소드
+    void run();
+}

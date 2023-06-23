d1f93e1b3812925daa621c9534e7f641d9b41fb0
gywns0417 <gywns0417@gmail.com>
2023-06-23 19:19:38 +0900

    인스턴스 멤버 클래스 예제

---
+++ src/NestedClass/InstanceClass/AExample.java
@@ -0,0 +1,10 @@
+package NestedClass.InstanceClass;
+
+public class AExample {
+    public static void main(String[] args) {
+        A a = new A();
+
+        //A 인스턴스 메소드 호출
+        a.useB();
+    }
+}

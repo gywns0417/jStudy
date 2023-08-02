57ca9218e3bad0993db097cc430936f9de349245
gywns0417 <gywns0417@gmail.com>
2023-08-02 18:50:08 +0900

    예외처리 확인문제

---
+++ src/Exception/Q7/NotExistIDExecption.java
@@ -0,0 +1,9 @@
+package Exception.Q7;
+
+public class NotExistIDExecption extends Exception {
+    public NotExistIDExecption(){}
+    public NotExistIDExecption(String message) {
+        super(message);
+    }
+    
+}
---
+++ src/Exception/Q8/FileWriterExample.java
@@ -0,0 +1,14 @@
+package Exception.Q8;
+
+import java.io.IOException;
+
+public class FileWriterExample {
+    public static void main(String[] args) {
+
+        try(FileWriter fw = new FileWriter("file.txt")) {
+            fw.write("Java");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

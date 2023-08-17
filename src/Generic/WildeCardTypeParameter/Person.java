139221ace0da4053534618a5d83e444499cfaf66
gywns0417 <gywns0417@gmail.com>
2023-08-17 16:19:44 +0900

    와일드카드 타입 파라미터 예제

---
+++ src/Generic/WildeCardTypeParameter/Person.java
@@ -0,0 +1,13 @@
+package Generic.WildeCardTypeParameter;
+
+public class Person {
+    
+}
+
+class Worker extends Person {}
+
+class Student extends Person{}
+
+class HighStudent extends Student{}
+
+class MiddleStudent extends Student{}

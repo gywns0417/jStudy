cbb069e5bc2866327ce12fab2f738cce10a5ad4e
gywns0417 <gywns0417@gmail.com>
2023-08-21 18:39:08 +0900

    스레드 생성 예제

---
+++ src/Thread/ThreadClass/BeepPrintExample.java
@@ -0,0 +1,30 @@
+package Thread.ThreadClass;
+
+import java.awt.Toolkit;
+
+public class BeepPrintExample {
+    public static void main(String[] args) {
+       Thread thread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            Toolkit toolkit = Toolkit.getDefaultToolkit(); // 툴킷 객체 얻기
+            for(int i = 0; i < 5; i++){
+                toolkit.beep();
+                try {
+                    Thread.sleep(500); 
+                } catch (Exception e ){}
+             }
+            }
+        });
+        
+        thread.start();
+
+       for(int i = 0; i < 5; i++){
+        System.out.println("띵");
+        try {
+            Thread.sleep(500);
+        } catch (Exception e) {
+        }
+       }
+    }
+}

60f1c42b6b1139b8b318734cfdf8acc7475e284b
gywns0417 <gywns0417@gmail.com>
2023-08-21 20:51:58 +0900

    스레드 상태 예제

---
+++ src/Thread/ThreadStatus/YieldExample.java
@@ -0,0 +1,24 @@
+package Thread.ThreadStatus;
+
+public class YieldExample {
+    public static void main(String[] args) {
+        WorkThread workThreadA = new WorkThread("workThreadA");
+        WorkThread workThreadB = new WorkThread("workThreadB");
+        workThreadA.start();
+        workThreadB.start();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // TODO: handle exception
+        }
+        workThreadA.work = false;
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            // TODO: handle exception
+        }
+        workThreadA.work = true;
+    }
+}

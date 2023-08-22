0ccd0a93cb81181cc04aa3402ad31d307437abaf
gywns0417 <gywns0417@gmail.com>
2023-08-22 17:42:46 +0900

    Set 사용 예제

---
+++ src/Collection/Set/HashSet/Member.java
@@ -0,0 +1,26 @@
+package Collection.Set.HashSet;
+
+public class Member {
+    public String name;
+    public int age;
+
+    public Member(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    // hashCode 재정의 (name, age가 같으면 동일한 hashCode 리턴)
+    public int hashCode(){
+        return name.hashCode() + age;
+    }
+
+    // equals 재정의
+    @Override
+    public boolean equals(Object obj){
+        if(obj instanceof Member target) {
+            return target.name.equals(name) && (target.age == age);
+        } else {
+            return false;
+        }
+    }
+}

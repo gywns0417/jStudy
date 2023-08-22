ac3a27ec0e6480d13358be7c2756635587dd09e3
gywns0417 <gywns0417@gmail.com>
2023-08-22 14:22:25 +0900

    스레드 동기화 예제

---
+++ src/Thread/ThreadSyn/ThreadB.java
@@ -0,0 +1,17 @@
+package Thread.ThreadSyn;
+
+public class ThreadB extends Thread{
+    private WorkObject workObject;
+
+    public ThreadB(WorkObject workObject){
+        setName("TthreadB");
+        this.workObject = workObject;
+    }
+
+    @Override
+    public void run(){
+        for(int i = 0; i < 10; i++){
+            workObject.methodB();
+        }
+    }
+}
